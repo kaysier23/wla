0d46a7389e86acdefc37c9d6e650459b591b1d5e
kaysier23 <60122621+kaysier23@users.noreply.github.com>
2020-03-20 12:43:23 +0800

    Update config.php

--- wla/config.php
+++ wla/config.php
@@ -9,13 +9,13 @@ require_once 'vendor/autoload.php';
 $google_client = new Google_Client();
 
 //Set the OAuth 2.0 Client ID
-$google_client->setClientId('539246690236-tvoad6op2k24t4010o2re4nd9v2kjq1n.apps.googleusercontent.com');
+$google_client->setClientId('302699445942-3unsom02e9b2hc1f69rmnm92g3biie1p.apps.googleusercontent.com');
 
 //Set the OAuth 2.0 Client Secret key
-$google_client->setClientSecret('WpZooC_GLJZXb9zunI3SH828');
+$google_client->setClientSecret('seLDKEPjBg6lTt_IV8maLV9_');
 
 //Set the OAuth 2.0 Redirect URI
-$google_client->setRedirectUri('https://maja-etact.herokuapp.com/index.php');
+$google_client->setRedirectUri('https://kyzier.herokuapp.com/index.php');
 
 //
 $google_client->addScope('email');
